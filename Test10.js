9d9fde66da6d6b1af7dddb6d67e46f7165f37b87
Abhishek Wathore <abhishekbwathore@gmail.com>
2024-10-23 23:43:24 +0530

    JS function-3

---
+++ Test10.js
@@ -0,0 +1,15 @@
+(function(){
+    console.log("Hello World!!!"); })();
+
+
+function greet(){
+    console.log("Good Morning");
+}
+
+// const greet1 =() =>{
+//     console.log("Good Afternoon");
+// };
+
+( ()=>{console.log("Hello");})();
+
+(function(x,y){console.log(x+y);})(10,20);
